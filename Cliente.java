293540b34ae5ef2ac16c6fdab5dadfd1c5a9cff2
Matheus Makita <matheus.makita@germinare.org.br>
2024-03-18 09:09:03 -0300

    Arrumando o cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,6 +1,11 @@
 public class Cliente {
     private String tipoConta;
     private String nome;
+
+    private String Matheus;
+
+    private  int numero;
+
     private  String cpf;
     private int agencia;
     private double saldo;
